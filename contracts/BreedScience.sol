0ffb9a6c18290a15f6774eb9f7a89373fc01d274
Lam <jason.lim@jswap.finance>
2021-10-21 14:47:00 +0800

    correct the breed rate

--- contracts/BreedScience.sol
+++ contracts/BreedScience.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 
 contract BreedScience  {
 
-    //@dev random % based < hitRate ? 1 : 0
+    //@dev random % based < hitRate ? 0 : 1
     //@return gender 0:female 1:male
     function breeedGender(uint256 based, uint256 hitRate) external view returns (uint256 hit) {
        uint256 random = uint256(keccak256(abi.encodePacked(tx.origin, block.difficulty, block.timestamp)));
-       return random % based <= hitRate ? 0 : 1;
+       return random % based < hitRate ? 0 : 1;
    }
 }
